ac21779c3d5e0e6a76e2472df2eb508eda7c4d6d
Justin Huang <JustinHuang016@gmail.com>
2022-10-15 15:19:27 -0400

    Finshed Problem3

--- Exercise5/Problem3.cpp
+++ Exercise5/Problem3.cpp
@@ -1,6 +1,67 @@
-//keep x and y totals
-//take sum of x and y
-//find mean x and y vals
 //find slope using 1st point of list with mean x & y vals
 //get intercept
 //get linear regression model?
+#include <iostream>
+#include <array>
+#include <vector>
+using namespace std;
+
+//X and Y points used
+vector <float> X = {30.0, 40.0, 70.0, 60.0, 80.0, 50.0};
+vector <float> Y = {8.0, 11.0, 12.0, 10.0, 15.0, 13.0};
+
+float Xmean=0.0, Ymean=0.0;
+
+
+float Slope(float Xmean, float Ymean){
+
+    float slope=0.0, Xsum=0.0, Ysum=0.0;
+    
+    float denom = 0.0;
+    float numer = 0.0;
+    //Denominator
+    for (int a=0; a < X.size(); a++){
+        denom += (X.at(a)-Xmean)*(X.at(a)-Xmean);
+    }
+    //Numerator
+        for (int a=0; a < X.size(); a++){
+        numer += (X.at(a)-Xmean)*(Y.at(a)-Ymean);
+    }
+
+    slope = numer/denom;
+
+    return slope;
+}
+
+float Mean(vector <float> point){
+
+    float temp=0.0;
+    float size=(float)point.size();
+    for(int i=0; i<point.size(); i++){
+        temp += point[i];
+    }
+
+    return temp/size;
+}
+// Calls Mean , Slope Function
+float Linear(){
+
+    Xmean = Mean(X);
+    Ymean = Mean(Y);
+    //Means are correct
+    float slope = Slope(Xmean,Ymean);
+
+    float Intercept = Ymean - (slope*Xmean);
+
+    cout << "The linear regresssion model is Y = " << (float)Intercept << " + " << (float)slope <<"X" << endl;
+
+
+    return 0;
+}
+
+
+int main(){
+
+Linear();
+
+}
